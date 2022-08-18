d458265a8ce74c15488ce54bb3477619fa1c5147
0seo8 <96939562+0seo8@users.noreply.github.com>
2022-08-18 12:53:10 +0900

    스토어 기본 구조 세팅

---
+++ src/feature/dustSlice.js
@@ -0,0 +1,13 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+  getSidoDatas: [],
+}
+
+export const dustSlice = createSlice({
+  name: 'dust',
+  initialState,
+  reducers: {},
+})
+
+export default dustSlice.reducer
