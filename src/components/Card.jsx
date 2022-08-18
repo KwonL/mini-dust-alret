ebec1d3254f5cee51b1c63eefa5e10c6d47b576d
0seo8 <96939562+0seo8@users.noreply.github.com>
2022-08-18 19:32:17 +0900

    카드컴포넌트 로직 완성

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,25 @@
 import React from 'react'
 
-function Card() {
-  return <div>Card</div>
+function Card({ datas }) {
+  return (
+    <div>
+      {datas &&
+        datas.map((item) => (
+          <div key={item.stationName}>
+            <div>
+              <h3>{item.stationName}</h3>
+              <span>{item.sidoName}</span>
+              <span>☆</span>
+            </div>
+            <div>
+              <p>{item.pm10Grade}</p>
+              <p>{item.khaiValue}</p>
+              <p>기준: {item.dataTime}</p>
+            </div>
+          </div>
+        ))}
+    </div>
+  )
 }
 
 export default Card
