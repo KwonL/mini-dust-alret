709034e22140ec9e530e63c5dafd1b293d0126cc
0seo8 <96939562+0seo8@users.noreply.github.com>
2022-08-18 14:58:55 +0900

    BrowserRouter 설정

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { BrowserRouter } from 'react-router-dom'
 import { store } from './app/store'
 import { Provider } from 'react-redux'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>,
 )
