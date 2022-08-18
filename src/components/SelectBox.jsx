be6882619ab72014af204e4f415960a649af30fa
0seo8 <96939562+0seo8@users.noreply.github.com>
2022-08-18 17:24:59 +0900

    selectbox영역 로직 구현

--- src/components/SelectBox.jsx
+++ src/components/SelectBox.jsx
@@ -1,7 +1,54 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { OPTIONS } from '../utils/constants'
+import {
+  getGuGunList,
+  getDustDataStatus,
+  getDustDataError,
+  fetchDatas,
+  filterGuGunDatas,
+} from '../feature/dustSlice'
+import { useSelector, useDispatch } from 'react-redux/es/exports'
 
 function SelectBox() {
-  return <div>SelectBox</div>
+  const [sido, setSido] = useState(OPTIONS[0])
+  const dispatch = useDispatch()
+  const isStatus = useSelector(getDustDataStatus)
+  const isError = useSelector(getDustDataError)
+  const guGunList = useSelector(getGuGunList)
+
+  const changeSidoHandler = async (e) => {
+    setSido(e.target.value)
+  }
+
+  const changeGuGunHandler = (e) => {
+    dispatch(filterGuGunDatas(e.target.value))
+  }
+
+  useEffect(() => {
+    if (isStatus === 'idle') {
+      dispatch(fetchDatas(sido))
+    }
+  }, [isStatus, dispatch, sido])
+
+  return (
+    <>
+      <select onChange={changeSidoHandler}>
+        {OPTIONS.map((sido, index) => (
+          <option key={index} value={sido}>
+            {sido}
+          </option>
+        ))}
+      </select>
+      <select onChange={changeGuGunHandler}>
+        {guGunList &&
+          guGunList.map((gugun, index) => (
+            <option key={index} value={gugun}>
+              {gugun}
+            </option>
+          ))}
+      </select>
+    </>
+  )
 }
 
 export default SelectBox
