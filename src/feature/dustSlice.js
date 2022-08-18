2cf96a162cb7b4cf94be0c0c6fbb669480a54b4b
0seo8 <96939562+0seo8@users.noreply.github.com>
2022-08-18 14:59:30 +0900

    createAsyncThunk작성

--- src/feature/dustSlice.js
+++ src/feature/dustSlice.js
@@ -1,13 +1,72 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import axios from 'axios'
+const { VITE_SERVICE_KEY } = import.meta.env
+const BASE_URL = 'api/B552584/ArpltnInforInqireSvc/getCtprvnRltmMesureDnsty'
+
+const getParameters = {
+  serviceKey: VITE_SERVICE_KEY,
+  returnType: 'json',
+  numOfRows: '100',
+  pageNo: '1',
+  ver: '1.0',
+}
 
 const initialState = {
-  getSidoDatas: [],
+  setSidoDatas: null,
+  setGuGunList: null,
+  status: 'idle',
+  error: null,
 }
+
+export const fetchDatas = createAsyncThunk(
+  'dust/fetchDatas',
+  async (sidoName) => {
+    try {
+      const { data } = await axios.get(BASE_URL, {
+        params: { ...getParameters, sidoName },
+      })
+      const response = data['response']['body']['items'].map((item) => {
+        return (item = {
+          ...item,
+          myFavorite: false,
+        })
+      })
+      const result = response.reduce((acc, cur) => {
+        return {
+          ...acc,
+          [cur.stationName]: [cur],
+        }
+      }, {})
+      return result
+    } catch (error) {
+      return error.message
+    }
+  },
+)
 
 export const dustSlice = createSlice({
   name: 'dust',
   initialState,
-  reducers: {},
+  reducers: {
+    setSidoDats: {},
+  },
+  extraReducers(builder) {
+    builder
+      .addCase(fetchDatas.pending, (state, action) => {
+        state.status = 'loading'
+      })
+      .addCase(fetchDatas.fulfilled, (state, action) => {
+        const result = action.payload
+        state.status = 'succeeded'
+        state.setSidoDatas = result
+        state.setGuGunList = Object.keys(result)
+        console.log(state)
+      })
+      .addCase(fetchDatas.rejected, (state, action) => {
+        state.status = 'failed'
+        state.error = action.error.message
+      })
+  },
 })
 
 export default dustSlice.reducer
