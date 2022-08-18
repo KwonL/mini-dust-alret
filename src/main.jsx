d365ecf7b685a2ad77dd1927f39094765551a756
0seo8 <96939562+0seo8@users.noreply.github.com>
2022-08-19 01:21:21 +0900

    스타일드컴포넌트, 리액트 아이콘즈 설치

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,6 @@ import App from './App'
 import { BrowserRouter } from 'react-router-dom'
 import { store } from './app/store'
 import { Provider } from 'react-redux'
-import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
