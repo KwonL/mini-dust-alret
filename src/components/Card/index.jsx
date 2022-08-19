408e4d3b7a051120c6c6888cdc3751ac2ba3de60
0seo8 <96939562+0seo8@users.noreply.github.com>
2022-08-19 15:04:54 +0900

    Card 스타일링 완성

---
+++ src/components/Card/index.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import * as S from './style'
+import { useDispatch, useSelector } from 'react-redux'
+import { getSidoDatas } from '../../feature/dustSlice'
+import { addToMyFavoriteList } from '../../feature/dustSlice'
+import { FaStar, FaRegStar } from 'react-icons/fa'
+import { strGrade } from '../../utils/constants'
+
+function Card({ guGun }) {
+  const dispatch = useDispatch()
+  const datas = useSelector(getSidoDatas)
+  const data = datas[guGun]
+
+  const addToMyFavorite = () => {
+    dispatch(addToMyFavoriteList(data.stationName))
+  }
+
+  const grade = strGrade(data.pm10Grade)
+
+  return (
+    <>
+      {data && (
+        <S.Card>
+          <S.CardHeader>
+            <S.CardTitle>
+              {data.stationName}
+              <S.SubTitle>{data.sidoName}</S.SubTitle>
+            </S.CardTitle>
+
+            <S.Star onClick={addToMyFavorite}>
+              {data.myFavorite ? (
+                <FaStar size="1.5rem" />
+              ) : (
+                <FaRegStar size="1.5rem" />
+              )}
+            </S.Star>
+          </S.CardHeader>
+          <S.CardBody>
+            <S.StrGradArea>{grade}</S.StrGradArea>
+            <S.Text>미세먼지수치 : {data.khaiValue}</S.Text>
+            <S.Text>({data.dataTime} 기준)</S.Text>
+          </S.CardBody>
+        </S.Card>
+      )}
+    </>
+  )
+}
+
+export default Card
---
+++ src/components/Card/style.js
@@ -0,0 +1,54 @@
+import styled from 'styled-components'
+
+export const Card = styled.div`
+  box-sizing: border-box;
+  transition: 0.3s ease box-shadow;
+  padding: 1rem 1.5rem;
+  border-radius: 0.5rem;
+  margin-top: 1.2rem;
+  background-color: #03c75a;
+  color: #fff;
+  // min-height: 13rem;
+`
+
+export const CardHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-start;
+`
+
+export const CardTitle = styled.div`
+  font-size: 1.2rem;
+  font-weight: bold;
+`
+
+export const SubTitle = styled.span`
+  margin-left: 0.5rem;
+  font-size: 1rem;
+  font-weight: normal;
+`
+
+export const Star = styled.div`
+  cursor: pointer;
+  padding: 0.5rem;
+  display: block;
+`
+
+export const CardBody = styled.div`
+  display: flex;
+  flex-flow: column;
+  align-items: center;
+`
+
+export const StrGradArea = styled.div`
+  background-color: #fff;
+  border-radius: 0.5rem;
+  color: #03c75a;
+  font-weight: bold;
+  display: inline-block;
+  padding: 0.5rem 1rem;
+  margin-top: 0.3rem;
+  margin-bottom: 0.8rem;
+  font-size: 1.1rem;
+`
+export const Text = styled.div``
